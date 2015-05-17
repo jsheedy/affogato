b13a0ad887b94e9d69d62115263b78b4fe95e3fc
Austin Gross <austingross@hotmail.com>
2015-05-16 22:47:39 -0700

    added switch for interaction

--- gelato/gelato.py
+++ gelato/gelato.py
@@ -8,12 +8,12 @@
 
 
 import pandas as pd
+import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
 
-def deseason(timeseries):
-    '''Accept iterator of dicts of timeseries and yield deseasonalized data'''
-
+def data_prep(timeseries):
+    '''Prepare timeseries data for analysis'''
     counter = pd.DataFrame(timeseries)
 
     counter['datetime'] = pd.to_datetime(counter.datetime)
@@ -22,7 +22,7 @@ def deseason(timeseries):
 
     counter['trend'] = counter.index
 
-    counter = counter[counter.notnull().any(axis=1)]
+    counter = counter[~counter.isnull().any(axis=1)]
 
     counter.set_index('datetime', inplace=True)
 
@@ -32,14 +32,32 @@ def deseason(timeseries):
 
     counter.reset_index(inplace=True)
 
-    fmla = 'inbound ~ trend + C(woy) + C(dow) + C(hr)'
+    return counter
+
+
+def deseason(timeseries, interact=False):
+    '''Accept iterator of dicts of timeseries and yield deseasonalized data'''
+    counter = data_prep(timeseries)
+
+    fmla = 'inbound ~ -1 + trend + '
+
+    if interact:
+        fmla += ' C(counter_id) * (C(woy) + C(dow) + C(hr))'
+    else:
+        fmla += ' C(counter_id) + C(woy) + C(dow) + C(hr)'
+
     result = smf.ols(formula=fmla, data=counter).fit()
 
     counter['fitted_inbound'] = result.predict()
     counter['residuals_inbound'] = result.resid
     counter['trend_inbound'] = result.params.trend * counter.trend
 
-    fmla = 'outbound ~ trend + C(woy) + C(dow) + C(hr)'
+    fmla = 'outbound ~ -1 + trend + '
+
+    if interact:
+        fmla += ' C(counter_id) * (C(woy) + C(dow) + C(hr))'
+    else:
+        fmla += ' C(counter_id) + C(woy) + C(dow) + C(hr)'
     result = smf.ols(formula=fmla, data=counter).fit()
 
     counter['fitted_outbound'] = result.predict()
@@ -52,3 +70,20 @@ def deseason(timeseries):
             'trend_inbound', 'trend_outbound']
 
     return counter[mask].to_dict('records')
+
+
+def pronto_effect(timeseries):
+    counter = data_prep(timeseries)
+
+    mask = counter.datetime >= '2014-10-13'
+
+    counter['post'] = 0
+    counter.ix[mask, 'post'] = 1
+
+    mask = [2, 8, 9]
+    mask = counter.counter_id.isin(mask)
+
+    counter['treat'] = 0
+    counter.ix[mask, 'treat'] = 1
+
+    return counter
